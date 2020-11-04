b60570f31c92dc1499d4509d6d3aad7e4fec44d5
Alexis <72479390+sciflybrarian@users.noreply.github.com>
2020-11-04 17:26:13 -0500

    Update 08rockpaperscissors.py

--- 08rockpaperscissors.py
+++ 08rockpaperscissors.py
@@ -1,7 +1,9 @@
 '''
 from https://www.practicepython.org/exercise/2014/03/26/08-rock-paper-scissors.html
 
-    Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
+    Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player
+    plays (using input), compare them, print out a message of congratulations
+    to the winner, and ask if the players want to start a new game)
 
     Remember the rules:
 
